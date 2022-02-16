264b7f74d65da8a8d923cffe6a9a2df9e707a2e6
vinezao <95624273+vinezao@users.noreply.github.com>
2022-02-16 15:39:52 -0300

    landing

---
+++ script.js
@@ -0,0 +1,23 @@
+if(document.URL.indexOf("moon.html") >= 0){ 
+    document.getElementsByClassName("divMoon")[0].style.backgroundColor = "#ffffff";
+    document.getElementsByClassName("moonA")[0].style.color ="#422D53"
+    document.getElementsByClassName("hdivMoon")[0].style.boxShadow = "2px 2px 2px 2px #9eddef";
+
+    }
+    if(document.URL.indexOf("mars.html") >= 0){ 
+        document.getElementsByClassName("divMars")[0].style.backgroundColor = '#ffffff';
+        document.getElementsByClassName("marsA")[0].style.color ="#422D53"
+        document.getElementsByClassName("divMars")[0].style.boxShadow = "2px 2px 2px 2px #9eddef";
+        }
+        if(document.URL.indexOf("jupiter.html") >= 0){ 
+            document.getElementsByClassName("divJupiter")[0].style.backgroundColor = "#ffffff";
+            document.getElementsByClassName("jupiterA")[0].style.color ="#422D53"
+            document.getElementsByClassName("divJupiter")[0].style.boxShadow = "2px 2px 2px 2px #9eddef";
+            }
+
+
+
+
+
+
+
